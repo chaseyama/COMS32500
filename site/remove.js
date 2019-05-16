8f01d5637c387fe950196c5bc3f83c6f49dff3ea
Jack Allison <jfallison@utexas.edu>
2019-05-16 12:15:35 +0100

    Updated Resource ejs

--- site/remove.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var sqlite = require("sqlite");
-remove();
-
-async function remove() {
-    var db = await sqlite.open("./users.db");
-    await db.run("delete from users where id=64");
-}
